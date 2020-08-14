315691aa25e664edae9175ea3235830179f28ebf
BenDavis71 <bendavis71@Knights.ucf.edu>
2020-08-14 17:40:37 -0500

    Fish .sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
-mkdir -p ~/.streamlit
-
-echo "\
-[server]\n\
-headless = true\n\
-port = $PORT\n\
-enableCORS = false\n\
-\n\
+mkdir -p ~/.streamlit/
+echo "[general]
+email = \"bendavis71@Knights.ucf.edu\"
+" > ~/.streamlit/credentials.toml
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
